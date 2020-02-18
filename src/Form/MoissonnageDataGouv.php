b891c790a47fa9d611692846364893484e17dec3
Kevin Monnery <kevin.monnery@bpm-conseil.com>
2020-02-18 09:12:30 +0000

    Update MoissonnageDataGouv.php

--- src/Form/MoissonnageDataGouv.php
+++ src/Form/MoissonnageDataGouv.php
@@ -1979,6 +1979,15 @@ class MoissonnageDataGouv extends HelpFormBase {
 							// read into array
 							//$arr = file('/home/user-client/drupal-d4c'.$filepath);
 							$arr = file($res->url); //error_log($arr);
+							if($arr == false || $arr == ""){error_log("retentative..");
+								$arrContextOptions=array(
+									"ssl"=>array(
+										"verify_peer"=>false,
+										"verify_peer_name"=>false,
+									),
+								);
+								$arr = file($res->url, 0, stream_context_create($arrContextOptions));
+							}
 							$label = utf8_decode($arr[0]);
 							$label = $this->nettoyage($label);  
 					
