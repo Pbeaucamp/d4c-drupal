843382ccd1efce2812d215a8d4ac830e9d4a909a
Kevin Monnery <kevin.monnery@bpm-conseil.com>
2020-07-20 09:10:54 +0000

    update moissonnage filtre

--- js/MoissonnageDataGouv.js
+++ js/MoissonnageDataGouv.js
@@ -1469,6 +1469,12 @@ function createTablePrew(resUrl,type_file,type_site){
 
  resUrl = resUrl.replace(/\//g,'!');
 
+//check if url contains any params
+if(resUrl.includes("?")) {
+      var res = resUrl.split("?");
+      resUrl = res[0];
+ }
+
 $.ajax('/datasets/update/getCsvXls/' + resUrl+';'+type_file+';'+type_site , {
 
         type: 'POST',
