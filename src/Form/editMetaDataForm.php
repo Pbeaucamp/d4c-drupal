761ffd17dabf719d12b6e6ec112a763626a9d4f5
Kevin Monnery <kevin.monnery@bpm-conseil.com>
2020-09-16 08:49:56 +0000

    GTFS script

--- src/Form/editMetaDataForm.php
+++ src/Form/editMetaDataForm.php
@@ -904,7 +904,6 @@ class editMetaDataForm extends HelpFormBase {
 
 
 		$result = $resourceManager->manageFileWithPath($datasetId, $generateColumns, $isUpdate, $resourceId, $resourceUrl, $description, $encoding);
-    var_dump($result);die;
 
         foreach ($result as $key => $value) {
 			if ($value['status'] == 'complete') {
