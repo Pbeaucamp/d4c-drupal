9bb2c90af72618681513bb2f4744b53004e51df7
Sebastien Vigroux <sebastien.vigroux@bpm-conseil.com>
2023-10-19 13:49:31 +0200

    Modify swagger file by cron to update with define gravitee URL in config

--- src/Utils/PropertiesHelper.php
+++ src/Utils/PropertiesHelper.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\ckan_admin\Utils;
 
+use Drupal\Component\Serialization\Yaml;
+use Exception;
+
 class PropertiesHelper {
 
 	const DATABASE_TABLE = 'd4c_properties';
@@ -103,5 +106,39 @@ class PropertiesHelper {
 				->key('key');
 	
 			$query->execute();
+	}
+
+	/**
+	 * This method is used to update the swagger file of the API.
+	 */
+	public function updateSwagger() {
+		$graviteeUrl = $this->config->gravitee->url;
+		$graviteeHeaderKey = $this->config->gravitee->header_key;
+		$graviteeApiKey = $this->config->gravitee->api_key;
+
+		Logger::logMessage("TRM - Gravity URL: $graviteeUrl");
+
+		// Check if gravitee URL is defined
+		if (!empty($graviteeUrl)) {
+			// We need to update the file openapi_data4citizen.yaml which is located in the current directory
+			$swaggerFile = __DIR__ . "/../../openapi_data4citizen.yaml";
+			$swaggerFileContent = file_get_contents($swaggerFile);
+
+			// Parse the YAML content
+			try {
+				$data = Yaml::decode($swaggerFileContent);
+
+				// Modify the servers URL
+				$data['servers'][0]['url'] = $graviteeUrl;
+	
+				// Convert the updated data back to YAML
+				$updatedYaml =Yaml::encode($data);
+	
+				// Save the modified content back to the file
+				file_put_contents($swaggerFile, $updatedYaml);
+			} catch (Exception $exception) {
+				Logger::logMessage('Unable to parse the YAML string: ' . $exception->getMessage());
+			}
+		}
 	}
 }
